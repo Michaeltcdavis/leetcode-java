51d6db391a71cdb36e61ad7e24a7983db8956097
Michael Davis <michaeltcdavis@gmail.com>
2025-04-21 13:17:54 -0400

    add comment

--- src/main/java/com/mtcd/leetcode/TwoSum.java
+++ src/main/java/com/mtcd/leetcode/TwoSum.java
@@ -6,6 +6,16 @@ import java.util.stream.Collectors;
 
 public class TwoSum {
 
+    /**
+     * Method takes an integer array and a target sum. The method returns an
+     * array of
+     * two values which add up to the target sum. If no two values exist, the
+     * method returns null.
+     *
+     * @param ints   The array of integers
+     * @param target The target sum
+     * @return The array of integers which add up to the target sum
+     */
     public static int[] twoSum(int[] ints, int target) {
         HashMap<Integer, Integer> cache = new HashMap<>();
         for (int i = 0; i < ints.length; i++) {
