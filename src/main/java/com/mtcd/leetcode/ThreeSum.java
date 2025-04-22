2949b5aaf6214c730734f78192bb6468d9c81256
Michael Davis <michaeltcdavis@gmail.com>
2025-04-22 19:16:37 -0400

    fix/ threesome reusing numbers for twosum

--- src/main/java/com/mtcd/leetcode/ThreeSum.java
+++ src/main/java/com/mtcd/leetcode/ThreeSum.java
@@ -7,32 +7,45 @@ public class ThreeSum {
 
     public static Set<List<Integer>> subTripletsWhichSum(int[] ints,
                                                          int target) {
-        HashSet<Integer> set = new HashSet<>();
+        List<Integer> used = new ArrayList<>();
         Set<List<Integer>> trips = new HashSet<>();
         for (int i : ints) {
             int remainder = target - i;
-            List<List<Integer>> twoSums = twoSum(set, remainder);
+            List<List<Integer>> twoSums = twoSum(used, remainder);
             for (List<Integer> pair : twoSums) {
                 pair.add(i);
                 Collections.sort(pair);
                 trips.add(pair);
             }
-            set.add(i);
+            used.add(i);
         }
         return trips;
     }
 
-    private static List<List<Integer>> twoSum(HashSet<Integer> ints,
+    private static List<List<Integer>> twoSum(List<Integer> ints,
                                               int target) {
         List<List<Integer>> result = new ArrayList<>();
         for (int i : ints) {
             int remainder = target - i;
-            if (ints.contains(remainder)) {
+            if (ints.contains(remainder) && notSameNum(i, remainder, ints)) {
                 List<Integer> twoSum = list(i, remainder);
                 result.add(twoSum);
             }
         }
         return result;
+    }
+
+    private static boolean notSameNum(int i, int remainder, List<Integer> ints) {
+        return i == remainder ? hasMultiple(ints, i) : true;
+    }
+
+    private static boolean hasMultiple(List<Integer> ints, int i) {
+        int count = 0;
+        for (int in : ints) {
+            if (in == i)
+                count++;
+        }
+        return count > 1;
     }
 
     private static List<Integer> list(int... ints) {
