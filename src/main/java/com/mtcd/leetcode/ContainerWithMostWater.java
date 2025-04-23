508e7bf7773ea688e5e1d5a76bf013e297b24fcd
Michael Davis <michaeltcdavis@gmail.com>
2025-04-22 21:37:00 -0400

    add largest water volume with pointers

--- src/main/java/com/mtcd/leetcode/ContainerWithMostWater.java
+++ src/main/java/com/mtcd/leetcode/ContainerWithMostWater.java
@@ -6,8 +6,6 @@ public class ContainerWithMostWater {
     public static int maxWaterVolume(int[] height) {
         //A = h * w (shortest)
         int max = 0;
-        int right = 0;
-        int left = 0;
         for (int i = 0; i < height.length - 1; i++) {
             int maxWidth = height.length - i;
             if ((maxWidth * height[i]) < max)
@@ -21,8 +19,26 @@ public class ContainerWithMostWater {
         return max;
     }
 
+    public static int maxWaterVolumePointers(int[] height) {
+        int max = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (right > left) {
+            int a = Math.min(height[right], height[left]) * (right - left);
+
+            if (a > max)
+                max = a;
+
+            if (height[right] > height[left])
+                left++;
+            else
+                right--;
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         int[] input = new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7};
-        System.out.println(maxWaterVolume(input));
+        System.out.println(maxWaterVolumePointers(input));
     }
 }
