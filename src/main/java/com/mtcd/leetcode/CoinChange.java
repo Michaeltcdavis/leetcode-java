06dd81c41636f34fbb0e8766b0cf1c32a4958223
Michael Davis <michaeltcdavis@gmail.com>
2025-04-23 13:01:15 -0400

    add minimum coins calculator

---
+++ src/main/java/com/mtcd/leetcode/CoinChange.java
@@ -0,0 +1,28 @@
+package com.mtcd.leetcode;
+
+import java.util.Arrays;
+
+public class CoinChange {
+
+    public static int leastCoinsUsed(int[] coinValues, int total) {
+        int[] amt = new int[total + 1];
+        Arrays.fill(amt, total + 1);
+        amt[0] = 0;
+        for (int i = 1; i <= total; i++) {
+            int min = total + 1;
+            for (int val : coinValues) {
+                if (i - val < 0)
+                    continue;
+                else
+                    min = Math.min(min, 1 + amt[i - val]);
+            }
+            amt[i] = min;
+        }
+        return amt[total] == (total + 1) ? -1 : amt[total];
+    }
+
+    public static void main(String[] args) {
+        int[] coinValues = new int[]{1, 3, 5, 6};
+        System.out.println(leastCoinsUsed(coinValues, 9));
+    }
+}
