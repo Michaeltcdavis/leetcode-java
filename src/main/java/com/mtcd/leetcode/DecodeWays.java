0ef6cc83329f09a6a4fe8a773907abde14fd72fd
Michael Davis <michaeltcdavis@gmail.com>
2025-04-28 14:15:16 -0400

    fix/ some zero cases

--- src/main/java/com/mtcd/leetcode/DecodeWays.java
+++ src/main/java/com/mtcd/leetcode/DecodeWays.java
@@ -11,15 +11,23 @@ public class DecodeWays {
         if (prev == 0)
             return 0; // no way to decode leading zero
 
+        boolean prevFree = false; // is prev available to group with prev - 1
         int max = 1;
         for (int i = 1; i < s.length(); i++) {
             int current = Character.getNumericValue(s.charAt(i));
 
-            if (current != 0)
+            if (current == 0) {
+                if (prev == 0)
+                    return 0;
+                else if (prevFree)
+                    max--;
+                prevFree = false;
+            } else if (prevFree) {
                 max++;
-            if ((prev == 1 || prev == 2) && current < 7)
+            } else if (prev == 1 || prev == 2 && current < 7) {
                 max++;
-
+                prevFree = true;
+            }
             prev = current;
         }
         return max;
@@ -27,7 +35,7 @@ public class DecodeWays {
     }
 
     public static void main(String[] args) {
-        String s = "12";
+        String s = "12024";
         System.out.println(waysToDecode(s));
     }
 }
