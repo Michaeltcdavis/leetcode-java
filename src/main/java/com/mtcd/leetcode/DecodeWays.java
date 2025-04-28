b37aa6ccec9b2426977e34cfb88d42095c5912d8
Michael Davis <michaeltcdavis@gmail.com>
2025-04-28 16:03:21 -0400

    fix/ summing all previous paths

--- src/main/java/com/mtcd/leetcode/DecodeWays.java
+++ src/main/java/com/mtcd/leetcode/DecodeWays.java
@@ -3,36 +3,32 @@ package com.mtcd.leetcode;
 public class DecodeWays {
 
     public static int waysToDecode(String s) {
-        int prev = Character.getNumericValue(s.charAt(0));
-        if (prev == 0)
+        if (s.charAt(0) == '0')
             return 0; // no way to decode leading zero
 
-        boolean prevFree = false; // is prev available to group with prev - 1
-        int max = 1;
-        for (int i = 1; i < s.length(); i++) {
-            int current = Character.getNumericValue(s.charAt(i));
+        int one = 1; //max ways to decode from i - 1
+        int two = 1; // max ways to decode from i - 2
 
-            if (current == 0) {
-                if (prev == 0 || prev > 2)
-                    return 0;
-                else if (prevFree) {
-                    max--;
-                    prevFree = false;
-                }
-            } else if (prevFree) {
-                max++;
-            } else if (prev == 1 || prev == 2 && current < 7) {
-                max++;
-                prevFree = true;
+        for (int i = 1; i < s.length(); i++) {
+            int current = 0;
+
+            if (s.charAt(i) != '0')
+                current = one; // current num can be treated on its own
+
+            int val = Integer.parseInt(s.substring(i - 1, i + 1));
+            if (val >= 10 && val <= 26) {
+                current += two; // current num can be combined with last
             }
-            prev = current;
+            two = one;
+            one = current;
+
         }
-        return max;
+        return one;
 
     }
 
     public static void main(String[] args) {
-        String s = "897078";
+        String s = "1043";
         System.out.println(waysToDecode(s));
     }
 }
