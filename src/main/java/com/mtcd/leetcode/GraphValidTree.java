ae2c5f91da06ec0e639c1f9d0264bb92476e7f68
Michael Davis <michaeltcdavis@gmail.com>
2025-05-12 17:43:10 -0400

    fix tree connected nodes check

--- src/main/java/com/mtcd/leetcode/GraphValidTree.java
+++ src/main/java/com/mtcd/leetcode/GraphValidTree.java
@@ -1,9 +1,19 @@
 package com.mtcd.leetcode;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Stack;
 
 public class GraphValidTree {
-
+    /**
+     * This method determines whether a list of graph edges represents a valid
+     * tree
+     *
+     * @param n     the number of nodes in the graph
+     * @param edges the links between nodes numbered 0 through n - 1
+     * @return true if the edges represent a valid tree, false otherwise
+     */
     public static boolean isValidTree(int n, int[][] edges) {
         // init hashmap
         // for each edge, check if child edge exists in map as key
@@ -20,21 +30,37 @@ public class GraphValidTree {
         // add all ints in edges to a set
         // confirm set is size n
         // confirm edges length is n - edges
-        HashSet<Integer> nodes = new HashSet<>();
-        for (int[] edge : edges) {
-            nodes.add(edge[0]);
-            nodes.add(edge[1]);
-        }
-        if (nodes.size() != n)
-            return false;
-
         if (edges.length != n - 1)
             return false;
-        return true;
+
+        List<List<Integer>> adjacency = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjacency.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            adjacency.get(edge[0]).add(edge[1]);
+            adjacency.get(edge[1]).add(edge[0]);
+        }
+        Stack<Integer> path = new Stack<>();
+        HashSet<Integer> visited = new HashSet<>();
+
+        path.push(0);
+        visited.add(0);
+
+        while (!path.isEmpty()) {
+            Integer node = path.pop();
+            for (Integer neighbour : adjacency.get(node)) {
+                if (visited.contains(neighbour))
+                    continue;
+                visited.add(neighbour);
+                path.push(neighbour);
+            }
+        }
+        return visited.size() == n;
     }
 
     public static void main(String[] args) {
-        int[][] input = new int[][]{{0, 1}, {1, 2}, {0, 2}};
+        int[][] input = new int[][]{{0, 1}, {1, 2}, {0, 3}};
         System.out.println(isValidTreeGT(4, input));
     }
 }
